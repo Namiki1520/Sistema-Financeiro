de079c88be6ecc187a9776fc5c3e8f59bb730eec
Namiki1520 <jairocsnamiki@hotmail.com>
2023-03-12 20:48:16 -0300

    Criadas as entidades e interfaces genericas

---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Entities.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase( DbContextOptions options ) : base(options)
+        {
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { set; get; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { set; get; }
+        public DbSet<Categoria> Categoria { set; get; }
+        public DbSet<Despesa> Despesa { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            //return "Data Source=.\SQLexpress;Initial Catalog=FINANCEIRO_2023;Integrated Security=False;User ID=sa;Password=asd;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+
+            return "Data Source=DESKTOP-42UINND;Initial Catalog=FINANCEIRO_2023;Integrated Security=True"; // Evitar
+        }
+
+
+    }
+}
