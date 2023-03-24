9f35711e0ea01b89c3d0f9c00e83e1525c946662
Namiki1520 <jairocsnamiki@hotmail.com>
2023-03-24 20:13:12 -0300

    Criadas as querys nos repositórios

--- Infra/Repositorio/ReposotorioDespesa.cs
+++ Infra/Repositorio/ReposotorioDespesa.cs
@@ -1,6 +1,8 @@
 ﻿using Domain.Interfaces.IDespesa;
 using Entities.Entidades;
+using Infra.Configuracao;
 using Infra.Repositorio.Generics;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +13,38 @@ namespace Infra.Repositorio
 {
     public class RepositorioDespesa : RepositoryGenerics<Despesa>, InterfaceDespesa
     {
-        public Task<IList<Despesa>> ListaDespesasNaoPagasMesAnterior(string emailUsuario)
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        public RepositorioDespesa()
         {
-            throw new NotImplementedException();
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+        public async Task<IList<Despesa>> ListaDespesasUsuario(string emailUsuario)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await
+                    (from s in banco.SistemaFinanceiro
+                     join c in banco.Categoria on s.Id equals c.IdSistema
+                     join us in banco.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                     join d in banco.Despesa on c.Id equals d.IdCategoria
+                     where us.EmailUsuario.Equals(emailUsuario) && s.Mes == d.Mes && s.Ano == d.Ano
+                     select d).AsNoTracking().ToListAsync();
+            }
+        }
+        public async Task<IList<Despesa>> ListaDespesasNaoPagasMesAnterior(string emailUsuario)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await
+                    (from s in banco.SistemaFinanceiro
+                     join c in banco.Categoria on s.Id equals c.IdSistema
+                     join us in banco.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                     join d in banco.Despesa on c.Id equals d.IdCategoria
+                     where us.EmailUsuario.Equals(emailUsuario) && d.Mes < DateTime.Now.Month && !d.Pago
+                     select d).AsNoTracking().ToListAsync();
+            }
         }
 
-        public Task<IList<Despesa>> ListaDespesasUsuario(string emailUsuario)
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
